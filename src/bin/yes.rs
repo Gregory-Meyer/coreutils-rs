c5dd2d84b476a51dea8700515374973e66796d5a
Gregory Meyer <gregjm@umich.edu>
2018-04-18 23:06:27 -0400

    add yes

--- src/bin/yes.rs
+++ src/bin/yes.rs
@@ -1,3 +1,33 @@
+extern crate clap;
+
+use std::io::Write;
+
+fn get_string() -> String {
+    "y\n".to_string()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let to_repeat = get_string();
+
+    let repeated: Vec<u8> = to_repeat.as_bytes()
+                                     .iter()
+                                     .cloned()
+                                     .cycle()
+                                     .take(4096)
+                                     .collect();
+
+    let stdout = std::io::stdout();
+    let mut lock = stdout.lock();
+
+    let ref slice = &repeated.as_slice();
+
+    loop {
+        match lock.write_all(slice) {
+            Ok(_) => (),
+            Err(e) => {
+                eprintln!("error while writing: {}", e);
+                std::process::exit(1);
+            }
+        }
+    }
 }
